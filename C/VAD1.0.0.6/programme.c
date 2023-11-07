1f214462fe4ec189152c1664abdf3e74aaff82c5
johanvadenne <johanvadenne@gmail.com>
2023-11-07 14:42:13 +0100

    1.0.0.4
    ça casse la tête mes sa avance

--- C/VAD1.0.0.6/programme.c
+++ C/VAD1.0.0.6/programme.c
@@ -8,13 +8,19 @@
 void verifieParametre(int nbrParam);
 char *declareChaine(const char *chaine);
 void affecteChaine(char **variable, const char *chaineAffecte);
-void verifieParametre(int nbrParam);
 void journal(char *chaine, ...);
 void erreur(char *chaine, ...);
+char *construitChaine(char *chaine, char **param);
+bool fichierExiste(const char *nomChemin);
+char *lectureFichier(const char *nomChemin);
 
 
 int main(int nbrParam, char *param[]) {
+    journal("@1", "", NULL);
     verifieParametre(nbrParam);
+    if (!fichierExiste(param[1])) { erreur("le fichier @1 est introuvable", param[1]); } else { journal("chemin \"@1\" trouve", param[1]); }
+    char *programmeVad = lectureFichier(param[1]);
+    journal(programmeVad);
 
     return 0;
 }
@@ -30,53 +36,75 @@ char *declareChaine(const char *chaine) {
     return nouvelleChaine;
 }
 
-void verifieParametre(int nbrParam)
-{
-
-    bool programmeOk;
-
-    // trop de praramètre
-    if (nbrParam > 2)
-    {
-        erreur("Il y a trop de parametre");
-        programmeOk = false;
-    }
-    // aucun paramètre
-    else if (nbrParam <= 1)
-    {
-        erreur("presiser le chemin du fichier à compiler, exemple:\n<compilateur.exe> <chemin/du/chichier.vad>");
-        programmeOk = false;
-    }
-    // OK
-    else
-    {
-        programmeOk = true;
-        journal("Contenue parametre : OK");
-    }
-}
 
 void journal(char *chaine, ...) {
     va_list vargs;
     va_start(vargs, chaine);
     int nbrParam = 0;
     char **tabParametre = NULL;
-    int allocMemoire = strlen(chaine);
 
     while (1) {
-        char *test2 = va_arg(vargs, char*);
-        if (test2 == NULL) {
+        char *param = va_arg(vargs, char*);
+        if (param == NULL) {
             break;
         }
         tabParametre = (char **)realloc(tabParametre, (nbrParam + 1) * sizeof(char *));
-        if (tabParametre != NULL) {
-            tabParametre[nbrParam] = (char *)malloc(strlen(test2) + 1);
+        if (tabParametre != NULL ) {
+            tabParametre[nbrParam] = (char *)malloc(strlen(param) + 1);
             if (tabParametre[nbrParam] != NULL) {
-                strcpy(tabParametre[nbrParam], test2);
+                strcpy(tabParametre[nbrParam], param);
                 nbrParam++;
             }
         }
     };
 
+    char *nouvelleChaine = construitChaine(chaine, tabParametre);
+    printf("%s\n", nouvelleChaine);
+
+    free(nouvelleChaine);
+    for (int i = 0; i < nbrParam; i++) {
+        free(tabParametre[i]);
+    }
+    free(tabParametre);
+    va_end(vargs);
+}
+
+void erreur(char *chaine, ...) {
+    va_list vargs;
+    va_start(vargs, chaine);
+    int nbrParam = 0;
+    char **tabParametre = NULL;
+
+    while (1) {
+        char *param = va_arg(vargs, char*);
+        if (param == NULL) {
+            break;
+        }
+        tabParametre = (char **)realloc(tabParametre, (nbrParam + 1) * sizeof(char *));
+        if (tabParametre != NULL ) {
+            tabParametre[nbrParam] = (char *)malloc(strlen(param) + 1);
+            if (tabParametre[nbrParam] != NULL) {
+                strcpy(tabParametre[nbrParam], param);
+                nbrParam++;
+            }
+        }
+    };
+
+    char *nouvelleChaine = construitChaine(chaine, tabParametre);
+    printf("%s\n", nouvelleChaine);
+
+    free(nouvelleChaine);
+    for (int i = 0; i < nbrParam; i++) {
+        free(tabParametre[i]);
+    }
+    free(tabParametre);
+    va_end(vargs);
+    exit(1);
+}
+
+char *construitChaine(char *chaine, char **tabParam) {
+
+    int allocMemoire = strlen(chaine);
     char *pos = strstr(chaine, "@");
     while (pos != NULL) {
         int numero;
@@ -86,11 +114,10 @@ void journal(char *chaine, ...) {
             numero = -1;
         }
 
-        if (numero >= 0 && numero < nbrParam) {
-            const char *argument = tabParametre[numero];
+        if (numero >= 0 && numero < sizeof(tabParam)) {
+            const char *argument = tabParam[numero];
             if (argument != NULL) {
                 allocMemoire += strlen(argument)-2;
-                printf("Argument trouvé : %s\n", argument);
             }
         }
         pos = strstr(pos + 1, "@");
@@ -103,8 +130,8 @@ void journal(char *chaine, ...) {
         if (chaine[i] == '@') {
             i++;
             int numArgument = atoi(&chaine[i]) - 1;
-            if (numArgument >= 0 && numArgument < nbrParam) {
-                const char *argument = tabParametre[numArgument];
+            if (numArgument >= 0 && numArgument < sizeof(tabParam)) {
+                const char *argument = tabParam[numArgument];
                 if (argument != NULL) {
                     strncat(nouvelleChaine, argument, strlen(argument));
                 } else {
@@ -118,12 +145,79 @@ void journal(char *chaine, ...) {
         }
     }
 
-    printf("%s\n", nouvelleChaine);
+    return nouvelleChaine;
 
-    free(nouvelleChaine);
-    for (int i = 0; i < nbrParam; i++) {
-        free(tabParametre[i]);
+}
+
+void verifieParametre(int nbrParam)
+{
+    bool programmeOk;
+
+    // trop de praramètre
+    if (nbrParam > 2)
+    {
+        erreur("Il y a trop de parametre");
+        programmeOk = false;
     }
-    free(tabParametre);
-    va_end(vargs);
+    // aucun paramètre
+    else if (nbrParam <= 1)
+    {
+        erreur("presiser le chemin du fichier a compiler");
+        programmeOk = false;
+    }
+    // OK
+    else
+    {
+        programmeOk = true;
+        journal("Contenue parametre : OK");
+    }
+}
+
+bool fichierExiste(const char *nomChemin)
+{
+    FILE *fichier = fopen(nomChemin, "r");
+    if (fichier != NULL)
+    {
+        fclose(fichier);
+        return true;
+    }
+    return false;
+}
+
+
+char *lectureFichier(const char *nomChemin)
+{
+    FILE *fichier;
+    char *contenueFichier = NULL;
+
+    fichier = fopen(nomChemin, "r");
+
+    if (fichier != NULL)
+    {
+        // Obtenir la taille du fichier
+        fseek(fichier, 0, SEEK_END);
+        long taille = ftell(fichier);
+        fseek(fichier, 0, SEEK_SET);
+
+        contenueFichier = (char *)malloc(taille + 1); // +1 pour le caractère nul
+
+        if (contenueFichier != NULL)
+        {
+            char *ligneProgrammeVad = (char *)malloc(taille + 1);
+            if (ligneProgrammeVad != NULL)
+            {
+                contenueFichier[0] = '\0'; // Initialise la chaîne vide
+
+                while (fgets(ligneProgrammeVad, taille, fichier))
+                {
+                    // Concaténez la ligne lue à la chaîne principale
+                    strcat(contenueFichier, ligneProgrammeVad);
+                }
+                free(ligneProgrammeVad);
+            }
+        }
+    }
+    fclose(fichier);
+
+    return contenueFichier;
 }
